4371d1d35f2792bce69758b4707144503863acc5
thach vu <vuhoangthach1402@gmail.com>
2023-05-22 23:05:34 +0700

    .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,48 +24,97 @@ const connectDB = async () => {
   
   connectDB();
 
-
-// Tạo schema cho task
+  // Tạo schema cho task
 const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
   deadline: { type: Date, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true },
   isCompleted: { type: Boolean, default: false },
 });
 
 const Task = mongoose.model('Task', taskSchema);
+const auditTrailSchema = new mongoose.Schema({
+  action: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  taskId: { type: mongoose.Schema.Types.ObjectId },
+  timestamp: { type: Date, default: Date.now }
+});
+
+const AuditTrail = mongoose.model('AuditTrail', auditTrailSchema);
+
+const logAuditTrail = async (action, userId, taskId) => {
+  const auditTrail = new AuditTrail({ action, userId, taskId });
+  await auditTrail.save();
+};
+
 
 const checkAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization;
     const decodedToken = jwt.verify(token, 'SECRET_KEY');
+    // console.log(decodedToken)
     req.userData = { userId: decodedToken.userId };
+    // console.log()
     next();
   } catch (error) {
     return res.status(401).json({ error: 'Xác thực thất bại' });
   }
 };
 
-app.use(express.json());
+const checkAPIKey = (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
 
-app.get('/tasks', checkAuth, async (req, res) => {
+  if (apiKey === 'daylaapikey') {
+    next(); // Cho phép tiếp tục xử lý các route
+  } else {
+    res.status(401).json({ error: 'API key không hợp lệ' });
+  }
+};
+
+app.use(express.json());
+app.use(checkAuth);
+app.use(checkAPIKey);
+
+// Lấy thông tin người dùng từ API
+const fetchUser = async (userId) => {
   try {
-    const tasks = await Task.find({ owner: req.userData.userId }).populate('owner');
-    res.json(tasks);
+    const response = await axios.get(`https://loginsystem.herokuapp.com/users/${userId}`, {
+      headers: {
+        'x-api-key': 'daylaapikey'
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Lỗi lấy thông tin người dùng từ API:', error);
+    throw new Error('Lỗi lấy thông tin người dùng từ API');
+  }
+};
+
+// Lấy danh sách tất cả các task của người dùng
+app.get('/tasks', async (req, res) => {
+  try {
+    const userId = req.userData.userId;
+    // console.log(userId)
+    const user = await fetchUser(userId);
+    const tasks = await Task.find({ owner: userId });
+    res.json({ user, tasks });
   } catch (error) {
     console.error('Lỗi lấy danh sách task:', error);
     res.status(500).json({ error: 'Lỗi lấy danh sách task' });
   }
 });
 
-app.get('/tasks/:taskId', checkAuth, async (req, res) => {
+// Lấy thông tin một task cụ thể
+app.get('/tasks/:taskId', async (req, res) => {
   try {
     const taskId = req.params.taskId;
-    const task = await Task.findOne({ _id: taskId, owner: req.userData.userId }).populate('owner');
+    const userId = req.userData.userId;
+    const user = await fetchUser(userId);
+    const task = await Task.findOne({ _id: taskId, owner: userId });
     if (!task) {
       res.status(404).json({ error: 'Không tìm thấy task' });
     } else {
-      res.json(task);
+      res.json({ user, task });
     }
   } catch (error) {
     console.error('Lỗi lấy thông tin task:', error);
@@ -72,11 +122,14 @@ app.get('/tasks/:taskId', checkAuth, async (req, res) => {
   }
 });
 
-app.post('/tasks', checkAuth, async (req, res) => {
+// Tạo một task mới
+app.post('/tasks', async (req, res) => {
   try {
     const { name, deadline } = req.body;
-    const task = new Task({ name, deadline, owner: req.userData.userId });
+    const userId = req.userData.userId;
+    const task = new Task({ name, deadline, owner: userId });
     const savedTask = await task.save();
+    await logAuditTrail('create', userId, savedTask._id);
     res.json(savedTask);
   } catch (error) {
     console.error('Lỗi tạo task:', error);
@@ -84,40 +137,40 @@ app.post('/tasks', checkAuth, async (req, res) => {
   }
 });
 
-app.delete('/tasks/:taskId', checkAuth, async (req, res) => {
+// Cập nhật một task
+app.put('/tasks/:taskId', async (req, res) => {
   try {
     const taskId = req.params.taskId;
-    const removedTask = await Task.findOneAndRemove({ _id: taskId, owner: req.userData.userId });
-    if (!removedTask) {
-      res.status(404).json({ error: 'Không tìm thấy task' });
-    } else {
-      res.json({ message: 'Task đã được xóa thành công' });
-    }
+    const { name, deadline } = req.body;
+    const userId = req.userData.userId;
+    const updatedTask = await Task.findOneAndUpdate(
+      { _id: taskId, owner: userId },
+      { name, deadline },
+      { new: true }
+    );
+    await logAuditTrail('update', userId, taskId);
+    res.json(updatedTask);
+  } catch (error) {
+    console.error('Lỗi cập nhật task:', error);
+    res.status(500).json({ error: 'Lỗi cập nhật task' });
+  }
+});
+
+// Xóa một task
+app.delete('/tasks/:taskId', async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const userId = req.userData.userId;
+    const removedTask = await Task.findOneAndRemove({ _id: taskId, owner: userId });
+    await logAuditTrail('delete', userId, taskId);
+    res.json({ message: 'Task đã được xóa thành công' });
   } catch (error) {
     console.error('Lỗi xóa task:', error);
     res.status(500).json({ error: 'Lỗi xóa task' });
   }
 });
 
-app.put('/tasks/:taskId', checkAuth, async (req, res) => {
-  try {
-    const taskId = req.params.taskId;
-    const { name, deadline } = req.body;
-    const updatedTask = await Task.findOneAndUpdate(
-      { _id: taskId, owner: req.userData.userId },
-      { name, deadline },
-      { new: true }
-    );
-    if (!updatedTask) {
-      res.status(404).json({ error: 'Không tìm thấy task' });
-    } else {
-      res.json(updatedTask);
-    }
-  } catch (error) {
-    console.error('Lỗi cập nhật task:', error);
-    res.status(500).json({ error: 'Lỗi cập nhật task' });
-  }
-});
+
 // Khởi động server
 app.listen(PORT, () => {
   console.log(`Server đang lắng nghe tại http://localhost:${PORT}`);
